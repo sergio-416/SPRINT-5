6bb609053dbfe6bbde0a567dd67c26804998c6ab
Sergio Morey <cmu_416@pm.me>
2025-11-24 19:35:42 +0100

    feat: implement image deletion functionality in Gallery component

--- src/app/components/gallery/gallery.ts
+++ src/app/components/gallery/gallery.ts
@@ -1,12 +1,12 @@
 import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
-import { Image } from '../../interfaces/image';
 import { ImageItem } from '../image-item/image-item';
+import { Image } from '../../interfaces/image';
 
 @Component({
   selector: 'app-gallery',
-  standalone: true,
   imports: [ImageItem],
   templateUrl: './gallery.html',
+  styleUrl: './gallery.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Gallery {
@@ -21,59 +21,70 @@ export class Gallery {
     },
     {
       id: 238,
-      author: 'Valentin Salja',
-      width: 2000,
-      height: 1333,
-      url: 'https://unsplash.com/photos/0aX51h4WvAk',
+      author: 'Fabrice Villard',
+      width: 3000,
+      height: 2000,
+      url: 'https://unsplash.com/photos/Du41jIaI5Ww',
       download_url: 'https://picsum.photos/id/238/300/200',
     },
     {
       id: 239,
-      author: 'Paul Jarvis',
-      width: 2000,
-      height: 1600,
-      url: 'https://unsplash.com/photos/6J--NXulQCs',
+      author: 'Garrett Sears',
+      width: 3000,
+      height: 2000,
+      url: 'https://unsplash.com/photos/ECoFf_q3tdQ',
       download_url: 'https://picsum.photos/id/239/300/200',
     },
     {
       id: 240,
-      author: 'Jonatan Pie',
-      width: 3944,
-      height: 2622,
-      url: 'https://unsplash.com/photos/wLsqlndgrKA',
+      author: 'Kelley Bozarth',
+      width: 3000,
+      height: 1800,
+      url: 'https://unsplash.com/photos/8Gq-aL7Nlps',
       download_url: 'https://picsum.photos/id/240/300/200',
     },
     {
       id: 241,
-      author: 'Alex Perez',
-      width: 2000,
-      height: 1333,
-      url: 'https://unsplash.com/photos/vSMcRlFkwVE',
+      author: 'Rachel Williams',
+      width: 3000,
+      height: 2000,
+      url: 'https://unsplash.com/photos/l5if0iQfV4c',
       download_url: 'https://picsum.photos/id/241/300/200',
     },
     {
       id: 242,
-      author: 'Phil Botha',
-      width: 4000,
-      height: 2667,
-      url: 'https://unsplash.com/photos/v8bSMXfwCcY',
+      author: 'Tim Gouw',
+      width: 3000,
+      height: 2000,
+      url: 'https://unsplash.com/photos/7CDFZsNM7Qg',
       download_url: 'https://picsum.photos/id/242/300/200',
     },
     {
       id: 243,
-      author: 'Mike Petrucci',
-      width: 3944,
-      height: 2622,
-      url: 'https://unsplash.com/photos/uIf6H1or1nE',
+      author: 'Darren Nunis',
+      width: 3000,
+      height: 2000,
+      url: 'https://unsplash.com/photos/bYDj8nE8lbo',
       download_url: 'https://picsum.photos/id/243/300/200',
     },
     {
       id: 244,
-      author: 'Mike Meyers',
-      width: 2000,
-      height: 1333,
-      url: 'https://unsplash.com/photos/3W7CKyMpgJM',
+      author: 'Shaun Bell',
+      width: 3000,
+      height: 2000,
+      url: 'https://unsplash.com/photos/C5pVBmGz58k',
       download_url: 'https://picsum.photos/id/244/300/200',
     },
   ]);
+
+  handleDelete(imageId: number): void {
+    const confirmed = window.confirm(
+      'Are you sure you want to delete this image? This action cannot be undone.'
+    );
+
+    if (confirmed) {
+      const updatedImages = this.images().filter((image) => image.id !== imageId);
+      this.images.set(updatedImages);
+    }
+  }
 }
